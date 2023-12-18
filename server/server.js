1cebcfb377640f5da0f195e668c0264a3a034154
Ampofo Emelia Nyarko <152034790+Nyarkoemelia@users.noreply.github.com>
2023-12-18 12:44:28 +0000

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,12 +1,29 @@
 
-const express = require('express')
-const app = express()
-const port = 3000
+class PatientDatabase:
+    def _init_(self):
+        self.patients = {
+            "001": {
+                "name": "Emelia AMPOFO",
+                "address": "123 Main Street",
+                "contact_info": "+1234567890",
+                # Exclude sensitive data like emergency number, logins, password, and gender
+            },
+            "002": {
+                "name": "Emelia Ampofo",
+                "address": "456 Elm Street",
+                "contact_info": "+9876543210",
+                # Exclude sensitive data
+            }
+        }
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+    def get_patient(self, patient_id):
+        return self.patients.get(patient_id, None)
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+# Usage example
+db = PatientDatabase()
+patient = db.get_patient("001")
+
+# Access specific data points, excluding sensitive ones
+print(f"Patient Name: {patient['name']}")
+print(f"Patient Address: {patient['address']}")
+print(f"Patient Contact: {patient['contact_info']}")
